06c800eb2667297a3e89fca3639aea360201ff15
Jsegel <segeljosh@gmail.com>
2020-01-23 23:20:15 -0600

    working on api routes and dealing with issues with js page

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,12 +1,18 @@
 var util = require("util");
 var fs = require("fs");
 var readFileAsync = util.promisify(fs.readFile);
+var writeFileAsync = util.promisify(fs.writeFile);
 
 module.exports = function(app){
     app.get("/api/notes", function(req, res){
         readFileAsync("Develop/db/db.json", "utf8")
             .then(function(data){
                 console.log(data);
+                res.send(data);
             })
+    });
+    app.post("/api/notes", function(req,res){
+      console.log(req.body)  
+      res.send("saved")
     })
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var htmlRoutes = require("./Develop/routes/htmlroutes");
 var apiRoutes = require("./Develop/routes/apiroutes");
+// var index = require("./Develop/public/assets/js/index");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -8,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 htmlRoutes(app);
 apiRoutes(app);
+// index(app);
 
 app.listen(PORT, function () {
     console.log('Server listening on: http://localhost:' + PORT);

6127c64ec0ac0dcf8bb6b3e8dded1287343d58fa
Jsegel <segeljosh@gmail.com>
2020-01-24 10:10:31 -0600

    making server public

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('./develop/public'))
 htmlRoutes(app);
 apiRoutes(app);
 // index(app);

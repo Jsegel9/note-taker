fc83b7bc031fb9ca6943e16dc21cbd1ba0df3392
Jsegel <segeljosh@gmail.com>
2020-01-23 22:20:56 -0600

    server running

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
-
+var htmlRoutes = require("./Develop/routes/htmlroutes")
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// htmlRoutes(app);
+htmlRoutes(app);
 // apiRoutes(app);
 
 app.listen(PORT, function () {
